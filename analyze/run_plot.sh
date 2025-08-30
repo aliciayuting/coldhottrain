1715bc5192d5419fba6414e88d0db0414bab759b
aliciayuting <yy354@cornell.edu>
2025-08-29 20:34:44 -0400

    updated dir for run plot

--- analyze/run_plot.sh
+++ analyze/run_plot.sh
@@ -1,10 +1,10 @@
 # Analyze weight
 
-  python analyze_weight_hotcold.py \
-    --ckpt_root /pscratch/sd/l/lsx/runs/opt67b_fsdp_gsm8k/ \
-    --ckpt_pattern "checkpoint-epoch-*" \
-    --include_final \
-    --outdir /pscratch/sd/l/lsx/yyt_runs/opt67b_fsdp_gsm8k/ \
-    --which both --norm l2 \
-    --hot_pct 95 --cold_pct 50 --per_layer_thresholds \
-    --plot
+python analyze_weight_coldhot.py \
+--ckpt_root /pscratch/sd/l/lsx/runs/opt67b_fsdp_gsm8k/ \
+--ckpt_pattern "checkpoint-epoch-*" \
+--include_final \
+--outdir /pscratch/sd/l/lsx/yyt_runs/opt67b_fsdp_gsm8k/ \
+--which both --norm l2 \
+--hot_pct 95 --cold_pct 50 --per_layer_thresholds \
+--plot
