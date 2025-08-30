4b5c9046be78345cd1834855158b3b4e10b64a45
aliciayuting <yy354@cornell.edu>
2025-08-29 20:18:59 -0400

    run shell script

--- analyze/run_plot.sh
+++ analyze/run_plot.sh
@@ -1,4 +1,10 @@
-# after training
-python plot_coldhot.py --dir "$OUTDIR" --tau 1e-5 --plot_A
-# or specify a particular step (matches the filename)
-python plot_coldhot.py --dir "$OUTDIR" --step 0000200
+# Analyze weight
+
+  python analyze_weight_hotcold.py \
+    --ckpt_root /pscratch/sd/l/lsx/runs/opt67b_fsdp_gsm8k/ \
+    --ckpt_pattern "checkpoint-epoch-*" \
+    --include_final \
+    --outdir /pscratch/sd/l/lsx/yyt_runs/opt67b_fsdp_gsm8k/ \
+    --which both --norm l2 \
+    --hot_pct 95 --cold_pct 50 --per_layer_thresholds \
+    --plot
