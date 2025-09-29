a25422e3c66f3a182fb0008ba5eedcce5c7486df
Jamal Hashim <jamalh110@gmail.com>
2025-09-29 13:40:24 -0400

    test

---
+++ benchmark/qwen/finetune_qwen_glue.py
@@ -0,0 +1,163 @@
+from datasets import load_dataset
+from transformers import AutoTokenizer, AutoModelForCausalLM, DataCollatorWithPadding, TrainingArguments, Trainer, DataCollatorForLanguageModeling, AutoModelForSequenceClassification
+import torch
+from gradient_callback import *
+from probe import *
+import hashlib
+import time
+import torch.distributed as dist
+from skip_gradient_callback import SkipGradientCallback
+import logging
+
+logging.basicConfig(
+        level=getattr(logging, os.environ.get('LOG_LEVEL', 'INFO').upper(), logging.INFO),
+        format="[%(levelname)s] %(message)s"
+    )
+
+MODEL = "Qwen/Qwen2.5-0.5B"
+DATASET = "sst2"
+NUM_LABELS = 2
+RUN_NAME = "base"
+_RUN_TS = time.strftime("%Y%m%d-%H%M%S")
+SCRATCH = os.getenv("SCRATCH", "/pscratch/sd/l/lsx")
+ZERO_BOTTOM_K_PERCENT = 0.5   # Zero bottom 50% of gradients
+ZERO_MODE = "neurons"         # Options: "weights" or "neurons"
+FREEZE_AFTER_EPOCHS = 1       # Choose bottom-k once after this many epochs
+VALIDATION_FRACTION = 0.1     # Hold out 10% for validation
+
+
+MODE="random"
+RANDOM_HOT_K_PERCENT = 1.0
+CHANGE_RANDOM_EVERY_ITERS = 100
+
+def safe_destroy():
+    if dist.is_available() and dist.is_initialized():
+        try:
+            # Optional but helpful to flush in-flight NCCL ops
+            dist.barrier()
+        except Exception:
+            pass
+        try:
+            dist.destroy_process_group()
+        except Exception:
+            pass
+
+
+# Load tokenizer & model
+tok = AutoTokenizer.from_pretrained(MODEL, use_fast=False)
+# If tokenizer has no pad token (common for causal LMs), set it:
+if tok.pad_token is None:
+    tok.pad_token = tok.eos_token
+    
+model = AutoModelForSequenceClassification.from_pretrained(
+    MODEL,
+    torch_dtype=torch.bfloat16,
+    num_labels=NUM_LABELS
+    # device_map="auto")
+)
+model.config.pad_token_id = tok.pad_token_id
+
+# Load Alpaca-52K dataset
+ds = load_dataset(DATASET)
+
+# Preprocess into prompt–response format
+def tokenize_function(examples):
+    return tok(
+        examples["sentence"],
+        padding="max_length",
+        truncation=True,
+        max_length=512
+    )
+
+tokenized_ds = ds.map(tokenize_function, batched=False)
+
+
+print(tokenized_ds)
+# Data collator
+data_collator = DataCollatorWithPadding(tokenizer=tok)
+
+
+# output_dir = f"/pscratch/sd/l/lsx/runs/{MODEL.replace('/', '_')}-{DATASET.replace('/', '_')}"
+output_dir = f"{SCRATCH}/jamal_runs/{MODEL.replace('/', '_')}-{DATASET.replace('/', '_')}-{RUN_NAME}-{_RUN_TS}"
+
+weight_out_dir = f"{output_dir}/weight_dump"
+# Training arguments
+args = TrainingArguments(
+    output_dir=f"{output_dir}/ckpt",
+    per_device_train_batch_size=16,
+    per_gpu_eval_batch_size=8,
+    gradient_accumulation_steps=2,
+    # gradient_accumulation_steps=1,
+    num_train_epochs=30,
+    learning_rate=2e-5,
+    # fp16=True,
+    bf16=True,
+    logging_steps=100,
+    save_strategy="epoch",
+    eval_strategy="epoch",
+    weight_decay=0.01,
+    #save_steps=100,
+    # save_total_limit=2,
+    ddp_find_unused_parameters=False,
+    # max_steps = 16,
+)
+
+trainer = Trainer(
+    model=model,
+    args=args,
+    train_dataset=tokenized_ds["train"],
+    eval_dataset=tokenized_ds["validation"],
+    data_collator=data_collator,
+)
+
+
+
+num_gpus = torch.cuda.device_count()
+num_samples = len(tokenized_ds["train"])
+global_batch = args.per_device_train_batch_size * args.gradient_accumulation_steps * max(1, num_gpus)
+iters_per_epoch = (num_samples + global_batch - 1) // global_batch
+print(f"#GPUs: {num_gpus}  Global batch: {global_batch}  Iters/epoch: {iters_per_epoch}")
+
+
+# dump_out_dir = f"/pscratch/sd/l/lsx/runs/{MODEL.replace("/", "_")}-{DATASET.replace('/', '_')}-grad_dump"
+dump_out_dir = f"{output_dir}/grad_dump"
+
+
+
+
+skipgradient_cb = SkipGradientCallback(
+    model=model,
+    zero_bottom_k_percent=ZERO_BOTTOM_K_PERCENT,
+    zero_mode=ZERO_MODE,
+    epoch_start_track=FREEZE_AFTER_EPOCHS-1,   # start tracking gradient norms after this many epochs
+    epoch_compute_masks=FREEZE_AFTER_EPOCHS,  # compute & fix masks at this epoch
+    use_cold_every_iters=20,
+    output_dir=output_dir,
+    mode=MODE,
+    random_hot_k_percent=RANDOM_HOT_K_PERCENT,
+    change_random_every_iters=CHANGE_RANDOM_EVERY_ITERS,
+)
+trainer.add_callback(skipgradient_cb)
+
+dump_cb = PerModuleGradDumper(
+    out_dir=dump_out_dir,
+    model=model,
+    capture_steps=100,
+    include_bias=True,
+    also_embeddings=True,  # set True if you also want embeddings/lm_head
+    weight_out_dir=weight_out_dir,
+)
+
+# trainer.add_callback(dump_cb)
+
+# probe_cb = Probe()
+# trainer.add_callback(probe_cb)
+
+
+
+# Start training
+trainer.train()
+
+
+
+safe_destroy()
