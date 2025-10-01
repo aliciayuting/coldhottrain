38547886be28d6d77f13e93a870fe4f77bb2e5c1
Jamal Hashim <jamalh110@gmail.com>
2025-10-01 12:22:41 -0400

    print optimizer

--- benchmark/qwen/skip_gradient_callback.py
+++ benchmark/qwen/skip_gradient_callback.py
@@ -235,7 +235,7 @@ class SkipGradientCallback(TrainerCallback):
             self._ddp_assert_neuron_masks_identical()
             self.save_masks(appendage=f"_{state.global_step}")
             optimizer = kwargs.get("optimizer", None)
-            if optimizer is not None and type(optimizer) == MaskedAdamW:
+            if optimizer is not None and type(optimizer.optimizer) == MaskedAdamW:
                 optimizer.set_mask_dict(self.neuron_masks, strict=True)
                 logger.info("SkipGradientCallback: set new random masks in optimizer.")
             else:
