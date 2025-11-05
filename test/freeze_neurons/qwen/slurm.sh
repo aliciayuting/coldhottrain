2f6d19ab242596b738eeb7095911fa2953bd723d
Jamal Hashim <jamalh110@gmail.com>
2025-11-05 18:47:02 -0500

    abc

--- test/freeze_neurons/qwen/slurm.sh
+++ test/freeze_neurons/qwen/slurm.sh
@@ -3,7 +3,7 @@
 #SBATCH -n 8                                 # Total number of cores requested
 #SBATCH --get-user-env                       # retrieve the users login environment
 #SBATCH --mem=32000                           # server memory requested (per node)
-#SBATCH -t 4:00:00                           # Time limit (hh:mm:ss)
+#SBATCH -t 8:00:00                           # Time limit (hh:mm:ss)
 #SBATCH --partition=default_partition       # Request partition
 #SBATCH --gres=gpu:nvidia_rtx_a6000:1                  # Type/number of GPUs needed
 
